7ffecde592c5c535a49ee34c1b2fb94692437862
Noah <mcbeasty11.nh@icloud.com>
2021-04-22 08:49:34 -0500

    mongoose and mongoDB connected

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,23 +1,33 @@
+const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
 exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show single bootcamp' });
+  res
+    .status(200)
+    .json({ success: true, msg: `Show single bootcamp ${req.params.id}` });
 };
 
 // @desc    Create new bootcamps
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-  res.send(req.body);
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 };
 
 // @desc    Update new bootcamps
@@ -32,8 +42,7 @@ exports.updateBootcamp = (req, res, next) => {
 // @desc    Delete new bootcamps
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  res.status(200).json({ success: true, data: {} });
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -16,6 +17,9 @@ const bootcamps = require('./routes/bootcamps');
 // Function call for express
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging Middleware (Morgan)
 // Only run if in development environment
 if (process.env.NODE_ENV === 'development') {
@@ -24,6 +28,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+// Middleware error handler function
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
